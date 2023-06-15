5e47e8813eea899c73d0c815a71c8d02f43a4e27
Jorge <jorgednavarroa@hotmail.com>
2023-06-15 04:28:25 -0300

    categorias y peliculas terminadas

--- cartelera.js
+++ cartelera.js
@@ -1,18 +1,47 @@
-let aventura = [
+let peliculas = [
   {
     nombre: "Soy Groot",
     imagen: "images/yo_soy_groot.jpg",
+    categoria: "aventura",
   },
   {
     nombre: "Zootopia",
     imagen: "images/zootopia.jpg",
+    categoria: "aventura",
   },
   {
     nombre: "Zootopia",
     imagen: "images/zootopia.jpg",
+    categoria: "romance",
   },
   {
-    nombre: "Zootopia",
-    imagen: "images/zootopia.jpg",
+    nombre: "Winnie Poh",
+    imagen: "images/winnie_pooh.jpg",
+    categoria: "aventura",
+  },
+  {
+    nombre: "UP",
+    imagen: "images/up.jpg",
+    categoria: "aventura",
+  },
+  {
+    nombre: "UP",
+    imagen: "images/up.jpg",
+    categoria: "terror",
+  },
+  {
+    nombre: "UP",
+    imagen: "images/up.jpg",
+    categoria: "terror",
+  },
+  {
+    nombre: "UP",
+    imagen: "images/up.jpg",
+    categoria: "terror",
+  },
+  {
+    nombre: "UP",
+    imagen: "images/up.jpg",
+    categoria: "terror",
   },
 ];
