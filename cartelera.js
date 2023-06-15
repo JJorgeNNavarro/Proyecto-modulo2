0a37b72d7dbc0db2a409e0bad8f721fc3687cffe
Jorge <jorgednavarroa@hotmail.com>
2023-06-15 00:15:36 -0300

    agregada funcion de cartelera y objetos de prueba

---
+++ cartelera.js
@@ -0,0 +1,18 @@
+let aventura = [
+  {
+    nombre: "Soy Groot",
+    imagen: "images/yo_soy_groot.jpg",
+  },
+  {
+    nombre: "Zootopia",
+    imagen: "images/zootopia.jpg",
+  },
+  {
+    nombre: "Zootopia",
+    imagen: "images/zootopia.jpg",
+  },
+  {
+    nombre: "Zootopia",
+    imagen: "images/zootopia.jpg",
+  },
+];
--- index.js
+++ index.js
@@ -10,3 +10,29 @@ window.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("resize", ponerAlturaOverlay);
 });
+
+let productos = JSON.parse(localStorage.getItem("aventura"));
+
+if (productos === null) {
+  productos = aventura;
+  localStorage.setItem("carritox", JSON.stringify(productos));
+}
+
+function crearTarjeta(arrayProductos, seccion) {
+  arrayProductos.forEach((producto) => {
+    let item = `
+    <div class="col-3 p-2 new-sand bg-white my-2 justify-content-center d-flex flex-column bg-transparent">
+      <img src="${producto.imagen}" class="img-fluid" alt="Imagen del ${producto.nombre}">
+    </div>
+    `;
+    seccion.innerHTML += item;
+  });
+}
+
+function renderAventura() {
+  let lugarhtml = document.getElementById("aventura");
+
+  crearTarjeta(aventura, lugarhtml);
+}
+
+renderAventura();
